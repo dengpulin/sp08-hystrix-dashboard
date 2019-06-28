fcbb073ace91cea75103c6be861a85d8295e7aa3
UID <UID@DESKTOP-DACJOCN>
2019-06-28 10:16:10 +0800

    12132

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -11,12 +11,11 @@ public class Item {
 	private Integer id;
 	private String name;
 	private Integer number;
-<<<<<<< HEAD
+
 	//3434575755675.0155
 	//1111
-}
-=======
+
 	//3434
 	//111154355363
 }
->>>>>>> branch 'master' of https://github.com/dengpulin/sp08-hystrix-dashboard.git
+
